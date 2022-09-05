d24f0a3fd5a7939f5ad9359fbefe6ff824b98d08
Esegovic <111624639+Esegovic@users.noreply.github.com>
2022-09-05 20:16:26 +0200

    Update sql.sql
    .

--- SQL/sql.sql
+++ SQL/sql.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS `esegovic_login` (
   `identifier` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL,
   `username` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `password` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `secretLey` varchar(4) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `secretKey` varchar(4) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`identifier`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
